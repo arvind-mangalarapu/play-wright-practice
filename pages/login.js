9caece395ef31ba36f66794b83f2454b5fbc137f
arvind-mangalarapu <arvind@able.do>
2024-06-11 16:27:11 +0530

    first commit

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+exports.LoginPage = class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usrname_textbox = page.getByLabel("Username");
+    this.password_textbox = page.getByLabel("Password");
+    this.login_button = page.getByRole("button", { name: " Login" });
+  }
+
+  async gotoLoginPage() {
+    await this.page.goto("https://the-internet.herokuapp.com/login");
+  }
+  async login(username, password) {
+    await this.usrname_textbox.fill(username);
+    await this.password_textbox.fill(password);
+    await this.login_button.click();
+  }
+};
